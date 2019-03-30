e9029897d5e647089ee967b271756c6644eb9f47
Hiral <hirpatel03@gmail.com>
2019-03-29 23:19:10 -0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+/// 
+namespace Hello_World
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles));
+            Console.ReadLine();
+
+        }
+    }
+}
